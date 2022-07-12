5c219c596ea81d66b51a6b1f83b9d7358e9e0520
nitram31 <nitram31300@gmail.com>
2022-07-12 14:52:18 +0200

    added slide selection

--- Reporting_GUI_V4.py
+++ Reporting_GUI_V4.py
@@ -14,9 +14,11 @@ it was made by a student learning python with the only goal of making something 
 """
 
 
-def analyse_file(path, variable):
-    file = pd.read_excel(path, sheet_name='Upgrade', header=10)
+def analyse_file(path, variable, sheet_name):
+    sheet_name = sheet_name.get()
+    file = pd.read_excel(path, sheet_name=sheet_name, header=10)
     choice = variable.get()
+
     body_dict = {}
 
     for i in range(1, 11):
@@ -137,88 +139,110 @@ def output_file(body_dict, choice):
             df.to_excel(writer)
 
 
-def get_options(path):
-    file = pd.read_excel(path, sheet_name='Upgrade', header=10)
+def get_options(path, sheet_name):
+    if isinstance(sheet_name, tkinter.StringVar):
+        sheet_name = sheet_name.get()
+
+    file = pd.read_excel(path, sheet_name=sheet_name, header=10)
     option_list = []
     for el in file['Project']:
         if el not in option_list and str(type(el)) != "<class 'float'>":
-            print(el)
             option_list.append(el)
     return option_list
 
 
+def get_slide(path):
+    file = pd.read_excel(path, sheet_name=None, header=10)
+    slide_list = list(file.keys())
+    return slide_list
+
+
 def main():
     root = Tk()
-    try:
-        mylabel2 = Label(root, text="")
+    variable = tkinter.StringVar(root)
+    variable.set('')
+    drop_down_menu = OptionMenu(root, variable, '')
+    sheet_name_variable = tkinter.StringVar(root)
+    sheet_name_variable.set('')
+    drop_down_menu_slide = OptionMenu(root, sheet_name_variable, '')
+    frame = LabelFrame(root, text="Excel file path")
+    frame.grid(row=2, column=0, padx=10, pady=50)
 
-        def myclick():
+    frame2 = LabelFrame(root, text="Select Excel file")
+    frame2.grid(row=2, column=1, padx=10, pady=10)
+    mypath = Entry(frame, width=50)
+
+    mypath.grid(row=2, column=0)
+    drop_down_menu_slide.grid(row=3, column=0)
+    drop_down_menu.grid(row=4, column=0)
+
+    mylabel2 = Label(root, text="")
+
+    def show_option(path, sheet_name):
+        dropdown_menu = drop_down_menu
+        dropdown_menu.destroy()
+        option_list = get_options(path, sheet_name)
+        if option_list:
+            variable.set(option_list[0])
+            dropdown_menu = OptionMenu(root, variable, *option_list)
+        else:
+            variable.set('')
+            dropdown_menu = OptionMenu(root, variable, '')
+
+        dropdown_menu.grid(row=4, column=0)
+
+    def myclick():
+
+        path = mypath.get()
+        if path != "":
+            body_dict, choice = analyse_file(path, variable, sheet_name_variable)
+            output_file(body_dict, choice)
+            button_message = "Everything went smoothly, the files should be in the folder " + \
+                             "from wich you executed the program"
+            mylabel2 = Label(root, text=button_message)
+            mylabel2.grid(row=6, column=0)
+
+    def myfile():
+        def OptionMenu_SelectionEvent(event):
+            show_option(path, event)
+
+        try:
+            root.fasta_file = filedialog.askopenfilename()
+            mypath.delete(first=0, last=tkinter.END)
+            mypath.insert(0, root.fasta_file)
             path = mypath.get()
-            if path != "":
-                body_dict, choice = analyse_file(path, variable)
-                print(body_dict['body7'])
-                output_file(body_dict, choice)
-                button_message = "Everything went smoothly, the files should be in the folder " + \
-                                 "from wich you executed the program"
-                mylabel2 = Label(root, text=button_message)
-                mylabel2.grid(row=5, column=0)
+            slide_list = get_slide(path)
+            sheet_name_variable.set(slide_list[0])
+            dropdown_menu_slide = drop_down_menu_slide
+            dropdown_menu_slide.destroy()
+            dropdown_menu_slide = OptionMenu(root, sheet_name_variable, *slide_list,
+                                             command=OptionMenu_SelectionEvent)
+            dropdown_menu_slide.grid(row=3, column=0)
+            show_option(path, sheet_name_variable)
+            mybutton = Button(root, text="Run scan", command=myclick, state="active")
+            mybutton.grid(row=5, column=0)
+            root.update()
+        except ValueError:
+            button_message = "something went wrong, the most likely cause for this error is that you selected " + \
+                             "the wrong type of file"
+            mylabel2 = Label(root, text=button_message)
+            mylabel2.grid(row=5, column=0)
 
-        def myfile():
-            try:
-                root.fasta_file = filedialog.askopenfilename()
-                mypath.delete(first=0, last=tkinter.END)
-                mypath.insert(0, root.fasta_file)
-                path = mypath.get()
-                option_list = get_options(path)
-                variable.set(option_list[0])
-                drop_down_menu = OptionMenu(root, variable, *option_list)
-                drop_down_menu.grid(row=3, column=0)
-                mybutton = Button(root, text="Run scan", command=myclick, state="active")
-                mybutton.grid(row=4, column=0)
-                root.update()
-            except ValueError:
-                button_message = "something went wrong, the most likely cause for this error is that you selected " + \
-                                 "the wrong type of file"
-                mylabel2 = Label(root, text=button_message)
-                mylabel2.grid(row=5, column=0)
+    mybutton = Button(root, text="Run scan", command=myclick, state="disabled")
+    mybutton2 = Button(frame2, text="Select file", command=myfile)
+    mybutton.grid(row=5, column=0)
+    mybutton2.grid(row=2, columns=2)
 
-        def paste(self):
-            self.entry.event_generate('<Control-v>')
-
-        def cut(self):
-            self.entry.event_generate('<Control-x>')
-
-        def copy(self):
-            self.entry.event_generate('<Control-c>')
-
-        frame = LabelFrame(root, text="Excel file path")
-        frame.grid(row=2, column=0, padx=10, pady=50)
-
-        frame2 = LabelFrame(root, text="Select Excel file")
-        frame2.grid(row=2, column=1, padx=10, pady=10)
-
-        mybutton = Button(root, text="Run scan", command=myclick, state="disabled")
-        mybutton2 = Button(frame2, text="Select file", command=myfile)
-
-        variable = tkinter.StringVar(root)
-        variable.set('')
-        drop_down_menu = OptionMenu(root, variable, '')
-
-        mypath = Entry(frame, width=50)
-
-        mypath.grid(row=2, column=0)
-        mybutton.grid(row=4, column=0)
-        mybutton2.grid(row=2, columns=2)
-        drop_down_menu.grid(row=3, column=0)
-
-        mylabel2.grid(row=2, column=1)
-
+    mylabel2.grid(row=2, column=1)
+    try:
         root.mainloop()
     except Exception as e:
         # please remove my email address if you took over this script
-        button_message = "Something went wrong, please send the errorlog that should have been created in the " + \
-                         "folder from wich you executed the program to martin.racoupeau@univ-tlse3.fr or the " + \
-                         "person maintaining the script"
+        email_address = 'martin.racoupeau@univ-tlse3.fr'
+        button_message = 'Something went wrong, please send the errorlog that should have been created in the ' \
+                         + 'folder from wich you executed the program to ' \
+                         + email_address \
+                         + ' or the person currently maintaining the script.'
 
         mylabel2 = Label(root, text=button_message)
         mylabel2.grid(row=5, column=0)
